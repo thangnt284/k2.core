6c1df4643368d03dc18b504a9c10f86f69f64bb9
KP <kenzy194@gmail.com>
2017-10-26 21:30:49 +0700

    up

---
+++ includes/functions.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @team: FsFlex Team
+ * @since: 1.0.0
+ * @author: KP
+ */
+function K2FileScanFromDirectory($dir, $mask, $options = array(), $depth = 0) {
+    $options += array(
+        'nomask' => '/(\.\.?|CSV)$/',
+        'callback' => 0,
+        'recurse' => TRUE,
+        'key' => 'uri',
+        'min_depth' => 0,
+    );
+
+    $options['key'] = in_array($options['key'], array('uri', 'filename', 'name')) ? $options['key'] : 'uri';
+    $files = array();
+    if (is_dir($dir) && $handle = opendir($dir)) {
+        while (FALSE !== ($filename = readdir($handle))) {
+            if (!preg_match($options['nomask'], $filename) && $filename[0] != '.') {
+                $uri = "$dir/$filename";
+                if (is_dir($uri) && $options['recurse']) {
+                    // Give priority to files in this folder by merging them in after any subdirectory files.
+                    $files = array_merge(cmsFileScanDirectory($uri, $mask, $options, $depth + 1), $files);
+                } elseif ($depth >= $options['min_depth'] && preg_match($mask, $filename)) {
+                    // Always use this match over anything already set in $files with the
+                    // same $$options['key'].
+                    $file = new stdClass();
+                    $file->uri = $uri;
+                    $file->filename = $filename;
+                    $file->name = pathinfo($filename, PATHINFO_FILENAME);
+                    $files[$filename] = $file;
+                }
+            }
+        }
+        closedir($handle);
+    }
+    return $files;
+}
---
+++ includes/shortcodes/K2ShortCode.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @team: K2 Team
+ * @since: 1.0.0
+ * @author: KP
+ */
+if(!defined('ABSPATH')){
+    exit();
+}
+if(!class_exists('K2ShortCode')){
+    class K2ShortCode extends WPBakeryShortCode{
+
+        protected function loadTemplate($atts, $content = null) {
+            $output = '';
+            $cms_template = isset($atts['cms_template']) ? $atts['cms_template'] : $this->shortcode.'.php';
+            $files = $this->findShortcodeTemplates();
+            if ($cms_template && isset($files[$cms_template])) {
+                $this->setTemplate($files[$cms_template]->uri);
+            } else {
+                $this->findShortcodeTemplate();
+            }
+            if (!is_null($content))
+                $content = apply_filters('vc_shortcode_content_filter', $content, $this->shortcode);
+            if ($this->html_template) {
+                ob_start();
+                include ( $this->html_template );
+                $output = ob_get_contents();
+                ob_end_clean();
+            } else {
+                trigger_error(sprintf(__('Template file is missing for `%s` shortcode. Make sure you have `%s` file in your theme folder.', 'js_composer'), $this->shortcode, 'wp-content/themes/your_theme/vc_templates/' . $this->shortcode . '.php'));
+            }
+            wp_reset_postdata();
+            wp_reset_query();
+            return apply_filters('vc_shortcode_content_filter_after', $output, $this->shortcode);
+        }
+
+        /**
+         *
+         * @return Array(): array of all avaiable templates
+         */
+        protected function findShortcodeTemplates() {
+            $theme_dir = get_template_directory() . '/vc_templates';
+            $child_dir = get_stylesheet_directory() . '/vc_templates';
+            $reg = "/^({$this->shortcode}\.php|{$this->shortcode}--.*\.php)/";
+            $files = K2FileScanFromDirectory($theme_dir, $reg);
+            $files = array_merge(K2FileScanFromDirectory(CMS_TEMPLATES, $reg), $files);
+            $files = array_merge($files, K2FileScanFromDirectory($child_dir, $reg));
+            return $files;
+        }
+    }
+}
